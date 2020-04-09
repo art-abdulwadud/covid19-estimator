8d972f44b268d72f773e9db67389553c504d7034
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-09 21:34:38 +0300

    challange one

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,22 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impactCI = data.reportedCases * 10;
-  const severeImpactCI = data.reportedCases * 50
+  const severeImpactCI = data.reportedCases * 50;
   let impactIBRT;
   let severeImpactIBRT;
-  if(data.periodType === 'days'){
-  	impactIBRT = impactCI * (Math.pow(2 ,Math.round(data.timeToElapse / 3)));
-  	severeImpactIBRT = severeImpactCI * (Math.pow(2 ,Math.round(data.timeToElapse / 3)));
+  if (data.periodType === 'days') {
+    impactIBRT = impactCI * (2 ** Math.round(data.timeToElapse / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.round(data.timeToElapse / 3));
   }
-  if(data.periodType === 'weeks'){
-  	let weeksToDays = data.timeToElapse * 7;
-  	impactIBRT = impactCI * (Math.pow(2 ,Math.round(weeksToDays / 3)));
-  	severeImpactIBRT = severeImpactCI * (Math.pow(2 ,Math.round(weeksToDays / 3)));
+  if (data.periodType === 'weeks') {
+    let weeksToDays = data.timeToElapse * 7;
+    impactIBRT = impactCI * (2 ** Math.round(weeksToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.round(weeksToDays / 3));
   }
-  if(data.periodType === 'months'){
-  	let monthsToDays = data.timeToElapse * 30;
-  	impactIBRT = impactCI * (Math.pow(2 ,Math.round(monthsToDays / 3)));
-  	severeImpactIBRT = severeImpactCI * (Math.pow(2 ,Math.round(monthsToDays / 3)));
+  if (data.periodType === 'months') {
+    let monthsToDays = data.timeToElapse * 30;
+    impactIBRT = impactCI * (2 ** Math.round(monthsToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.round(monthsToDays / 3));
   }
   return {
     data: input,
