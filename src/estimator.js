079833fb8cbfb75a0109b7108c8ca1bf5a5a20f7
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-09 23:27:05 +0300

    challange two

--- src/estimator.js
+++ src/estimator.js
@@ -20,8 +20,8 @@ const covid19ImpactEstimator = (data) => {
   }
   const impactSCBRT = impactIBRT * 0.15;
   const severeImpactSCBRT = severeImpactIBRT * 0.15;
-  const impactHBBRT = Math.floor(data.totalHospitalBeds * 0.35) - impactSCBRT;
-  const severeImpactHBBRT = Math.floor(data.totalHospitalBeds * 0.35) - severeImpactSCBRT;
+  const impactHBBRT = Math.round(data.totalHospitalBeds * 0.35) - impactSCBRT;
+  const severeImpactHBBRT = Math.round(data.totalHospitalBeds * 0.35) - severeImpactSCBRT;
   return {
     data: input,
     impact: {
