f22074a090826d3be3a0d1369443b12e570383bd
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-09 22:26:13 +0300

    challange two

--- src/estimator.js
+++ src/estimator.js
@@ -6,17 +6,17 @@ const covid19ImpactEstimator = (data) => {
   let severeImpactIBRT;
   if (data.periodType === 'days') {
     impactIBRT = impactCI * (2 ** Math.round(data.timeToElapse / 3));
-    severeImpactIBRT = severeImpactCI * (2 ** Math.round(data.timeToElapse / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.floor(data.timeToElapse / 3));
   }
   if (data.periodType === 'weeks') {
     const weeksToDays = data.timeToElapse * 7;
     impactIBRT = impactCI * (2 ** Math.round(weeksToDays / 3));
-    severeImpactIBRT = severeImpactCI * (2 ** Math.round(weeksToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.floor(weeksToDays / 3));
   }
   if (data.periodType === 'months') {
     const monthsToDays = data.timeToElapse * 30;
     impactIBRT = impactCI * (2 ** Math.round(monthsToDays / 3));
-    severeImpactIBRT = severeImpactCI * (2 ** Math.round(monthsToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.floor(monthsToDays / 3));
   }
   const impactSCBRT = impactIBRT * 0.15;
   const severeImpactSCBRT = severeImpactIBRT * 0.15;
