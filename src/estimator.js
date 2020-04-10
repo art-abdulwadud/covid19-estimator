ff852db5e21eef8d5f41e239b18e1186fac56ee9
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-10 14:31:55 +0300

    challange three

--- src/estimator.js
+++ src/estimator.js
@@ -10,30 +10,30 @@ const covid19ImpactEstimator = (data) => {
     impactIBRT = impactCI * (2 ** Math.floor(data.timeToElapse / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(data.timeToElapse / 3));
     impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * data.timeToElapse;
-    severeImpactDIF = severeImpactIBRT * 0.65 * data.avgDailyIncomeInUSD * data.timeToElapse;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * data.timeToElapse;
   }
   if (data.periodType === 'weeks') {
     const weeksToDays = data.timeToElapse * 7;
     impactIBRT = impactCI * (2 ** Math.floor(weeksToDays / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(weeksToDays / 3));
     impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * weeksToDays;
-    severeImpactDIF = severeImpactIBRT * 0.65 * data.avgDailyIncomeInUSD * weeksToDays;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * weeksToDays;
   }
   if (data.periodType === 'months') {
     const monthsToDays = data.timeToElapse * 30;
     impactIBRT = impactCI * (2 ** Math.floor(monthsToDays / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(monthsToDays / 3));
     impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * monthsToDays;
-    severeImpactDIF = severeImpactIBRT * 0.65 * data.avgDailyIncomeInUSD * monthsToDays;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * monthsToDays;
   }
   const impactSCBRT = impactIBRT * 0.15;
   const severeImpactSCBRT = severeImpactIBRT * 0.15;
   const impactHBBRT = Math.ceil(data.totalHospitalBeds * 0.35) - impactSCBRT;
   const severeImpactHBBRT = Math.ceil(data.totalHospitalBeds * 0.35) - severeImpactSCBRT;
-  const impactCFICU = impactIBRT * 0.5;
-  const severeImpactCFICU = severeImpactIBRT * 0.5;
-  const impactCFV = impactIBRT * 0.2;
-  const severeImpactCFV = impactIBRT * 0.2;
+  const impactCFICU = impactIBRT * 0.05;
+  const severeImpactCFICU = severeImpactIBRT * 0.05;
+  const impactCFV = impactIBRT * 0.02;
+  const severeImpactCFV = impactIBRT * 0.02;
   return {
     data: input,
     impact: {
