a2d77c5cb2bcb12534aaa09051e71af99bffe205
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-10 17:22:46 +0300

    challange three

--- src/estimator.js
+++ src/estimator.js
@@ -4,19 +4,27 @@ const covid19ImpactEstimator = (data) => {
   const severeImpactCI = data.reportedCases * 50;
   let impactIBRT;
   let severeImpactIBRT;
+  let impactDIF;
+  let severeImpactDIF;
   if (data.periodType === 'days') {
     impactIBRT = impactCI * (2 ** Math.floor(data.timeToElapse / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(data.timeToElapse / 3));
+    impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * data.timeToElapse;
   }
   if (data.periodType === 'weeks') {
     const weeksToDays = data.timeToElapse * 7;
     impactIBRT = impactCI * (2 ** Math.floor(weeksToDays / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(weeksToDays / 3));
+    impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * weeksToDays;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * weeksToDays;
   }
   if (data.periodType === 'months') {
     const monthsToDays = data.timeToElapse * 30;
     impactIBRT = impactCI * (2 ** Math.floor(monthsToDays / 3));
     severeImpactIBRT = severeImpactCI * (2 ** Math.floor(monthsToDays / 3));
+    impactDIF = impactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * monthsToDays;
+    severeImpactDIF = severeImpactIBRT * 0.65 * data.region.avgDailyIncomeInUSD * monthsToDays;
   }
   const impactSCBRT = impactIBRT * 0.15;
   const severeImpactSCBRT = severeImpactIBRT * 0.15;
@@ -26,6 +34,8 @@ const covid19ImpactEstimator = (data) => {
   const severeImpactCFICU = Math.trunc(severeImpactIBRT * 0.05);
   const impactCFV = Math.trunc(impactIBRT * 0.02);
   const severeImpactCFV = Math.trunc(severeImpactIBRT * 0.02);
+  impactDIF = Math.trunc(impactDIF);
+  severeImpactDIF = Math.trunc(severeImpactDIF);
   return {
     data: input,
     impact: {
@@ -35,7 +45,7 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: impactHBBRT,
       casesForICUByRequestedTime: impactCFICU,
       casesForVentilatorsByRequestedTime: impactCFV,
-      dollarsInFlight: 0
+      dollarsInFlight: impactDIF
     },
     severeImpact: {
       currentlyInfected: severeImpactCI,
@@ -44,7 +54,7 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: severeImpactHBBRT,
       casesForICUByRequestedTime: severeImpactCFICU,
       casesForVentilatorsByRequestedTime: severeImpactCFV,
-      dollarsInFlight: 0
+      dollarsInFlight: severeImpactDIF
     }
   };
 };
