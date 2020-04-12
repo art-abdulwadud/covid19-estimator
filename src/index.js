1907b519c212db97340d860d5a1a815a71a58915
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-04-12 10:59:42 +0300

    frontend markup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import covid19ImpactEstimator from './estimator';
+const regionName = document.querySelector('[data-region-name]');
+const avgAge = document.querySelector('[data-avg-age]');
+const avgDailyIncomeInUSD = document.querySelector('[data-avg-daily-income]');
+const avgDailyIncomePopulation = document.querySelector('[data-avg-daily-income-p]');
+const periodType = document.querySelector('[data-period-type]');
+const timeToElapse = document.querySelector('[data-time-to-elapse]');
+const reportedCases = document.querySelector('[data-reported-cases]');
+const population = document.querySelector('[data-population]');
+const totalHospitalBeds = document.querySelector('[data-total-hospital-beds]');
+const estimateBtn = document.querySelector('[data-go-estimate]');
+const inputTags = document.querySelectorAll('input[type="number"]');
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+const getInputData = () => {
+  const data = {
+    region: {
+      name: capitalize(regionName.value),
+      avgAge: avgAge.value,
+      avgDailyIncomeInUSD: avgDailyIncomeInUSD.value,
+      avgDailyIncomePopulation: avgDailyIncomePopulation.value
+    },
+    periodType: periodType.options[periodType.selectedIndex].value,
+    timeToElapse: timeToElapse.value,
+    reportedCases: reportedCases.value,
+    population: population.value,
+    totalHospitalBeds: totalHospitalBeds.value
+  };
+  console.log(data);
+};
+estimateBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if(avgAge.value != "" && regionName.value != "" 
+  	&& avgDailyIncomePopulation.value != "" && avgDailyIncomeInUSD.value != "" 
+  	&& timeToElapse.value != "" && reportedCases.value != "" && population.value != "" 
+  	&& totalHospitalBeds.value != ""){
+    getInputData();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
+	mode: 'development',
 	entry: './src/index.js',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			}
-		]
 	}
-};
+}
